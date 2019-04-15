36fa17e70e461e2db927f73af8986f3cb98d6108
laras126 <laras126@gmail.com>
2019-04-14 20:19:02 -0700

    fix: remove shortcode filters

--- functions.php
+++ functions.php
@@ -12,21 +12,3 @@ function c4aa_enqueue_assets() {
 }
 
 add_action( 'wp_enqueue_scripts', 'c4aa_enqueue_assets' );
-
-add_action('the_content', 'zole_remove_unused_shortcode');
-
-function zole_remove_unused_shortcode($content) { 
-	$pattern = '\\b([^\\]\\/]*(?:\\/(?!\\])[^\\]\\/]*)*?)(?:(\\/)\\]|\\](?:([^\\[]*+(?:\\[(?!\\/\\2\\])[^\\[]*+)*+)\\[\\/\\2\\])?)(\\]?)';
-	$matches = preg_match_all( '/'. $pattern .'/s', 'strip_shortcode_tag', $content );
-	print_r( $matches );
-}
-
-function zole_get_unused_shortcode_regex() {
-	global $shortcode_tags;
-	$tagnames = array_keys($shortcode_tags);
-	$tagregexp = join( '|', array_map('preg_quote', $tagnames) );
-	$regex = '\\[(\\[?)';
-	$regex .= "(?!$tagregexp)";
-	$regex .= '\\b([^\\]\\/]*(?:\\/(?!\\])[^\\]\\/]*)*?)(?:(\\/)\\]|\\](?:([^\\[]*+(?:\\[(?!\\/\\2\\])[^\\[]*+)*+)\\[\\/\\2\\])?)(\\]?)';
-	return $regex;
-}
