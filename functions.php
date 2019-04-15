e8bbd9487e58c589a8dd4490bf0c76ad9d022771
laras126 <laras126@gmail.com>
2019-04-14 20:13:43 -0700

    feat: enqueue stylesheets, add basics

--- functions.php
+++ functions.php
@@ -3,6 +3,12 @@
 function c4aa_enqueue_assets() {
 	wp_enqueue_style( 'twentynineteen-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'c4aa-style', get_stylesheet_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'c4aa-style-settings', get_stylesheet_directory_uri() . '/css/00-settings.css' );
+	wp_enqueue_style( 'c4aa-style-elements', get_stylesheet_directory_uri() . '/css/02-elements.css' );
+	wp_enqueue_style( 'c4aa-style-generic', get_stylesheet_directory_uri() . '/css/03-generic.css' );
+	wp_enqueue_style( 'c4aa-style-objects', get_stylesheet_directory_uri() . '/css/04-objects.css' );
+	wp_enqueue_style( 'c4aa-style-algorithms', get_stylesheet_directory_uri() . '/css/05-algorithms.css' );
+	wp_enqueue_style( 'c4aa-style-utilities', get_stylesheet_directory_uri() . '/css/06-utilities.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'c4aa_enqueue_assets' );
