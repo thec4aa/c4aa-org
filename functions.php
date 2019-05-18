ec23118d7e135bb0cb0b99d5f20bb4b408d97f0a
laras126 <laras126@gmail.com>
2019-05-18 12:06:59 -0400

    chore: update enqueue paths

--- functions.php
+++ functions.php
@@ -6,9 +6,8 @@ function c4aa_enqueue_assets() {
 	wp_enqueue_style( 'c4aa-style-settings', get_stylesheet_directory_uri() . '/css/00-settings.css' );
 	wp_enqueue_style( 'c4aa-style-elements', get_stylesheet_directory_uri() . '/css/02-elements.css' );
 	wp_enqueue_style( 'c4aa-style-generic', get_stylesheet_directory_uri() . '/css/03-generic.css' );
-	wp_enqueue_style( 'c4aa-style-objects', get_stylesheet_directory_uri() . '/css/04-objects.css' );
-	wp_enqueue_style( 'c4aa-style-algorithms', get_stylesheet_directory_uri() . '/css/05-algorithms.css' );
-	wp_enqueue_style( 'c4aa-style-utilities', get_stylesheet_directory_uri() . '/css/06-utilities.css' );
+	wp_enqueue_style( 'c4aa-style-c4aa-imageFilter', get_stylesheet_directory_uri() . '/css/c4aa-imageFilter.css' );
+	wp_enqueue_style( 'c4aa-style-c4aa-titleEffect', get_stylesheet_directory_uri() . '/css/c4aa-titleEffect.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'c4aa_enqueue_assets' );
