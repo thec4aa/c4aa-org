554fcd1058bfae62549ad6b39dc2d9858f4f866b
Steve Lambert <67271+slambert@users.noreply.github.com>
2019-06-28 14:35:41 -0400

    forgot to add the function
    🙄

--- functions.php
+++ functions.php
@@ -56,31 +56,34 @@ add_filter( 'body_class', 'c4aa_add_body_class_from_acf_options' );
  * via: https://kinsta.com/blog/twenty-nineteen-theme/#block-color-palettes
  * also added in css
  */
+function c4aa_setup_theme_supported_features() {
+    add_theme_support( 'editor-color-palette', array(
+        array(
+            'name' => __( 'C4AA Beige', 'themeLangDomain' ),
+            'slug' => 'c4aa-beige',
+            'color' => '#e8e6df',
+        ),
+        array(
+            'name' => __( 'C4AA Red', 'themeLangDomain' ),
+            'slug' => 'c4aa-red',
+            'color' => '#ef3340',
+        ),
+        array(
+            'name' => __( 'C4AA Grey', 'themeLangDomain' ),
+            'slug' => 'c4aa-grey',
+            'color' => '#968c83',
+        ),
+        array(
+            'name' => __( 'C4AA Yellow', 'themeLangDomain' ),
+            'slug' => 'c4aa-yellow',
+            'color' => '#e1cd00',
+        ),
+        array(
+            'name' => __( 'C4AA Blue', 'themeLangDomain' ),
+            'slug' => 'c4aa-blue',
+            'color' => '#005eb8',
+        ),
+    ) 
+    ); // end add_theme_support
+} // end c4aa_setup_theme_supported_features
 
-add_theme_support( 'editor-color-palette', array(
-    array(
-        'name' => __( 'C4AA Beige', 'themeLangDomain' ),
-        'slug' => 'c4aa-beige',
-        'color' => '#e8e6df',
-    ),
-    array(
-        'name' => __( 'C4AA Red', 'themeLangDomain' ),
-        'slug' => 'c4aa-red',
-        'color' => '#ef3340',
-    ),
-    array(
-        'name' => __( 'C4AA Grey', 'themeLangDomain' ),
-        'slug' => 'c4aa-grey',
-        'color' => '#968c83',
-    ),
-    array(
-        'name' => __( 'C4AA Yellow', 'themeLangDomain' ),
-        'slug' => 'c4aa-yellow',
-        'color' => '#e1cd00',
-    ),
-    array(
-        'name' => __( 'C4AA Blue', 'themeLangDomain' ),
-        'slug' => 'c4aa-blue',
-        'color' => '#005eb8',
-    ),
-) );
