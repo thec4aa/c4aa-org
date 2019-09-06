c9995fdf4b0312f0fd54d48e328133c736aa83f0
laras126 <laras126@gmail.com>
2019-09-06 09:58:23 -0400

    feat: echo svg in footer

--- footer.php
+++ footer.php
@@ -17,17 +17,15 @@
 
 	<footer id="colophon" class="site-footer">
 		<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
-		<div class="site-info">
+		<div class="site-info u-flex u-align-items-center">
+			<div class="u-width-50 u-display-inline-block">
+				<?php echo file_get_contents( get_stylesheet_directory() . '/svg/cc.logo.circle.svg' ); ?>
+			</div>
 			<?php $blog_info = get_bloginfo( 'name' ); ?>
 			<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
+				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 			<?php endif; ?>
-			<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
-				<?php
-				/* translators: %s: WordPress. */
-				printf( __( 'Proudly powered by %s.', 'twentynineteen' ), 'WordPress' );
-				?>
-			</a> -->
+			
 			<?php
 			if ( function_exists( 'the_privacy_policy_link' ) ) {
 				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
