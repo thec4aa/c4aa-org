05178b5d3181457a24f3422e126d4c759a278971
Steve Lambert <67271+slambert@users.noreply.github.com>
2019-09-07 11:42:25 -0400

    feat: 3col footer with the magic hand
    Created a 3 column footer by copying techniques used for gutenberg columns, including text alignment.
    
    Placed magic hand in the middle.

--- footer.php
+++ footer.php
@@ -17,33 +17,51 @@
 
 	<footer id="colophon" class="site-footer">
 		<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
-		<div class="site-info u-flex u-align-items-center">
-			<div class="u-width-50 u-display-inline-block">
-				<?php echo file_get_contents( get_stylesheet_directory() . '/svg/cc.logo.circle.svg' ); ?>
+		<div class="site-info u-flex u-align-items-center wp-block-columns has-3-columns">
+
+			<div class="wp-block-column" style="flex-basis:42.5%">
+
+				<div class="u-display-inline-block">
+					<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><?php echo file_get_contents( get_stylesheet_directory() . '/svg/cc.logo.circle.svg' ); ?></a>
+				</div>
+				<?php $blog_info = get_bloginfo( 'name' ); ?>
+				<?php if ( ! empty( $blog_info ) ) : ?>
+					<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				<?php endif; ?>
+
 			</div>
-			<?php $blog_info = get_bloginfo( 'name' ); ?>
-			<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			<?php endif; ?>
-			
-			<?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-			}
-			?>
-			<?php if ( has_nav_menu( 'footer' ) ) : ?>
-				<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'twentynineteen' ); ?>">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'menu_class'     => 'footer-menu',
-							'depth'          => 1,
-						)
-					);
-					?>
-				</nav><!-- .footer-navigation -->
-			<?php endif; ?>
+				
+			<div class="wp-block-column has-text-align-center" style="flex-basis:15%">
+
+				<div class="u-width-100 u-display-inline-block">
+					<?php echo file_get_contents( get_stylesheet_directory() . '/svg/c4aa.magic.hand.svg' ); ?>
+				</div>
+
+			</div>
+
+			<div class="wp-block-column has-text-align-right" style="flex-basis:42.5%">
+
+				<?php
+				if ( function_exists( 'the_privacy_policy_link' ) ) {
+					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+				}
+				?>
+
+				<?php if ( has_nav_menu( 'footer' ) ) : ?>
+					<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'twentynineteen' ); ?>">
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'footer',
+								'menu_class'     => 'footer-menu',
+								'depth'          => 1,
+							)
+						);
+						?>
+					</nav><!-- .footer-navigation -->
+				<?php endif; ?>
+			</div>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
