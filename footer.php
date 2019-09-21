25841689dcfece0977324c1493e6f9b70a13fb95
Steve Lambert <67271+slambert@users.noreply.github.com>
2019-09-21 13:50:00 -0400

    style: removing automatic privacy policy link
    We'll add our own privacy policy link via the footer menu, thanks. And one that just says "privacy" to save some space.

--- footer.php
+++ footer.php
@@ -42,12 +42,6 @@
 
 			<div class="footer-menu-column">
 
-				<?php
-				if ( function_exists( 'the_privacy_policy_link' ) ) {
-					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-				}
-				?>
-
 				<?php if ( has_nav_menu( 'footer' ) ) : ?>
 					<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'twentynineteen' ); ?>">
 						<?php
