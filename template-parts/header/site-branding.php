7c75adb8b47af44a335908c3ddd86b775a962116
Mike Mella <mike@belikewater.ca>
2023-11-03 11:14:39 -0400

    Added search icon in main nav

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -43,6 +43,15 @@
 				)
 			);
 			?>
+
+			<a href="https://c4aa.org/search" class="search-button-icon" aria-label="Search">
+				<svg fill="none" stroke="black" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img">
+					<title>Search</title>
+					<desc>A magnifying glass icon representing a search function</desc>
+					<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+				</svg>
+			</a>
+
 		</nav><!-- #site-navigation -->
 	<?php endif; ?>
 	<?php if ( has_nav_menu( 'social' ) ) : ?>
