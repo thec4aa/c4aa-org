996b4a42899ecf5a916ece72f358652b02b912f7
Mike Mella <mike@belikewater.ca>
2023-11-17 16:18:28 -0500

    Social icons header

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -7,6 +7,39 @@
  * @since 1.0.0
  */
 ?>
+<header class="social-header">
+	<a href="/newsletter" class="">Newsletter</a><!-- / -->
+	<ul class="social-header__list">
+		<li class="social-header__list-item">
+			<a href="" class="social-header__icon-link">
+				<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-x" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+				<path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+				<path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+				</svg>
+			</a><!-- / -->
+		</li><!-- / -->
+		<li class="social-header__list-item">
+			<a href="" class="social-header__icon-link">
+				<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-youtube" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+				<path d="M2 8a4 4 0 0 1 4 -4h12a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-12a4 4 0 0 1 -4 -4v-8z" />
+				<path d="M10 9l5 3l-5 3z" />
+				</svg>
+			</a><!-- / -->
+		</li><!-- / -->
+		<li class="social-header__list-item">
+			<a href="" class="social-header__icon-link">
+				<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+				<path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+				<path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+				<path d="M16.5 7.5l0 .01" />
+				</svg>
+			</a><!-- / -->
+		</li><!-- / -->
+	</ul><!-- / -->
+</header><!-- / -->
 
 <header class="mobile-header">
 	<a href="/" class="">
