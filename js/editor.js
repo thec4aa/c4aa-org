dc244c293c5586a9f1c47be684eb0ec2cb3fe168
Steve Lambert <67271+slambert@users.noreply.github.com>
2024-05-20 20:51:47 -0400

    feat: new button styles
    I tried to style these as much as I could with theme.json, but it just wasn't going to happen with the T19 theme. If I can figure it out later, I'll transfer over some of the styles, but for now it's all in css.
    
    These commits are a record of that journey...
    
    commit f2a6a9fde4234077a89e9b5db2783e6b0e145ced
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Mon May 20 20:48:32 2024 -0400
    
        feat: styling all the buttons
    
        high emphasis, low emphasis, and no emphasis. Animation works.
        https://codepen.io/slambert-the-decoder/pen/vYMbapM
    
    commit 9c2761537cd1ac8c0bbe7c4c994bcd862d9f4e6f
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Mon May 20 20:47:56 2024 -0400
    
        kill my dreams of theme.json button styles, for now.
    
        they're not working
    
    commit 127822f26769d9b53d565f0f65694e4d8266304e
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Mon May 20 19:15:22 2024 -0400
    
        ref: json
    
        cleaned up the json
    
    commit fdb066d610bdd4fd38c4b6aff64abbd3795a721b
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Mon May 20 19:14:53 2024 -0400
    
        progress on button styles
    
    commit e812fbdfa107b2e2031b8c952ad835a5cf3c27e6
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Sat May 18 08:27:43 2024 -0400
    
        wip elements vs. block styles
    
        buttons aren't being overridden yet. mystery.
    
    commit 7eb2c9e7126b75a2ce22d77abc4598e96dbcb0b9
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Fri May 17 18:49:28 2024 -0400
    
        chore: updating json schema
    
        certificate problems using .local, this solves it.
    
    commit 69f1dacc7d97c218beb2fec901c5b738944a0a25
    Merge: a38a9bd 96bb1ef
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Fri May 17 17:24:39 2024 -0400
    
        Merge branch 'develop' into feat-new-buttons
    
    commit a38a9bdd08fe3566987dfd16fde047590a70aac2
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Fri May 17 17:15:41 2024 -0400
    
        chore: midway through new buttons
    
        removing old clip path code, adding some basic styles, and adding new interface elements.
    
        Committing more because I need to sync with my other machine. Will probably squash this later.
    
    commit 9d48b46a180425f23d7019765ce6023defedd3ba
    Merge: 9ea543e f96e3ae
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Fri May 17 14:16:13 2024 -0400
    
        Merge pull request #315 from thec4aa/develop
    
        Add Basic Editor Styles
    
    commit 9ea543ebafe2d64c93780630049d5feb616a4373
    Merge: 338a0e7 d9a6249
    Author: Steve Lambert <67271+slambert@users.noreply.github.com>
    Date:   Wed May 15 18:30:37 2024 -0400
    
        Merge pull request #314 from thec4aa/develop
    
        Feat: Featured Image Duotone Color Options + Post Title Banner Options

--- js/editor.js
+++ js/editor.js
@@ -1,24 +1,23 @@
 wp.domReady( () => {
 
-	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
-	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
+	wp.blocks.unregisterBlockStyle('core/button', 'outline');
+	wp.blocks.unregisterBlockStyle('core/button', 'fill');
+
+	wp.blocks.registerBlockStyle('core/button', {
+		name: 'high-emphasis-button',
+		label: 'High Emphasis',
+		isDefault: true
+	} );
+
+	wp.blocks.registerBlockStyle('core/button', {
+		name: 'low-emphasis-button',
+		label: 'Low Emphasis'
+	} );
 
 	wp.blocks.registerBlockStyle( 'core/heading', {
 		name: 'c4aa-clipPath-heading',
 		label: 'C4AA Clip Path',
 		isDefault: true,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/button', {
-		name: 'c4aa-clipPath-button',
-		label: 'C4AA Clip Path Default',
-		isDefault: false,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/button', {
-		name: 'c4aa-clipPath-button-outline',
-		label: 'C4AA Clip Path Outline',
-		isDefault: false,
 	} );
 
 });
