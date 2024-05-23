34019287577a4d41bdc9c2eecc07a9846a0e1fe2
Steve Lambert <67271+slambert@users.noreply.github.com>
2024-05-22 21:05:46 -0400

    feat: clicks scroll to the title of the page

--- js/scrolling.js
+++ js/scrolling.js
@@ -9,31 +9,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });  
   
-  // this script  will smoothly scroll down one view height when the user clicks on either `.site-featured-image` and `.entry-content > :first-child.wp-block-pullquote` elements, and includes an exception for clicks on an anchor tag link within those elements.
+  // watches for clicks on either `.site-featured-image` and `.entry-content > :first-child.wp-block-pullquote` elements, and includes an exception for clicks on an anchor tag link within those elements. Once a click is detected, the function will scroll to the title of the page.
   // written by github copilot and modified by me.
-  
-  // select elements that will trigger the smooth scroll
-  const triggerElements = document.querySelectorAll('.site-featured-image, .entry-content > :first-child.wp-block-pullquote');
-  
-  // add click event listeners to trigger elements
-  triggerElements.forEach( element => {
-    element.addEventListener('click', (e) => {
-      // if the click target is an anchor tag, then do nothing
-      if ( e.target.tagName === 'A' ) {
-        return;
-      }
-      
-      // prevent default behavior
-      e.preventDefault();
-      
-      // scroll to the next view height
-      window.scrollBy({
-        top: window.innerHeight,
-        behavior: 'smooth'
+    document.addEventListener('click', function (event) {
+        if (event.target.closest('.site-featured-image, .entry-content > :first-child.wp-block-pullquote') && !event.target.closest('a')) {
+        document.querySelector('.entry-header h1.entry-title').scrollIntoView({
+            behavior: 'smooth'
         });
+        }
     });
-  });
-
+  
 
 
 
