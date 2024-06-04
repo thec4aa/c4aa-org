23bd5d478b60e7088ddb63dcaa7534a95e22fd3f
Steve Lambert <67271+slambert@users.noreply.github.com>
2024-06-03 21:01:41 -0500

    feat: main portrait block improvements
    fix: no minimum portrait size
    
    just recommended.
    
    feat: style details no top margin
    
    ref: move main portrait style into block
    
    feat: portrait image renders square

--- blocks/person-main-portrait/render.php
+++ blocks/person-main-portrait/render.php
@@ -3,7 +3,7 @@
 // Information added via custom fields on the Person Post Type
     $image = get_field( 'person-main-portrait', get_the_ID() );
     if ( $image ) : ?>
-    <figure class="testimonial__image">
-        <?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'testimonial__img' ) ); ?>
+    <figure class="person_figure__image">
+    <a href="<?php echo esc_url( get_permalink());?>" class="person-img-link"><?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'person__img' ) ); ?></a>
     </figure>
 <?php endif; ?>
