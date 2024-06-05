e4a069e61e50db3729ec57b544195482ad4b848a
Steve Lambert <67271+slambert@users.noreply.github.com>
2024-06-05 11:21:55 -0500

    feat: removing main portrait bc featured image now works.

--- blocks/person-main-portrait/render.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Displays primary portrait of the person
-// Information added via custom fields on the Person Post Type
-    $image = get_field( 'person-main-portrait', get_the_ID() );
-    if ( $image ) : ?>
-    <figure class="person_figure__image">
-    <a href="<?php echo esc_url( get_permalink());?>" class="person-img-link"><?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'person__img' ) ); ?></a>
-    </figure>
-<?php endif; ?>
