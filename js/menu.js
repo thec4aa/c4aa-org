766abb48f70a0cb65d6f25013f222ec44f5d4380
Steve Lambert <67271+slambert@users.noreply.github.com>
2024-07-02 00:30:21 -0400

    fix: no error on same page
    when on the search page, it would give an js error

--- js/menu.js
+++ js/menu.js
@@ -66,24 +66,23 @@ document.addEventListener("DOMContentLoaded", function() {
       // Get the parent li element of the selected span
       var menuItem = spanElement.closest('li');
       
-      // // Hide the menu item initially to prevent flashing
-      // menuItem.style.visibility = 'hidden';
-      
-      // Path to the new SVG file
-      var searchSVG = `
-      <svg class="svg-icon" width="24" height="24" aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-      <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"/>
-      </svg>
-      `;
-      
-      // Find the existing SVG within the menu item
-      var existingSVG = menuItem.querySelector("svg");
-      
-      // Replace the existing SVG with the new SVG
-      if (existingSVG) {
-        existingSVG.outerHTML = searchSVG;
-        // Show the menu item now that the new SVG is loaded
-        menuItem.classList.add('svg-loaded');
+      if (menuItem) {
+        // Path to the new SVG file
+        var searchSVG = `
+        <svg class="svg-icon" width="24" height="24" aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+        <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"/>
+        </svg>
+        `;
+        
+        // Find the existing SVG within the menu item
+        var existingSVG = menuItem.querySelector("svg");
+        
+        // Replace the existing SVG with the new SVG
+        if (existingSVG) {
+          existingSVG.outerHTML = searchSVG;
+          // Show the menu item now that the new SVG is loaded
+          menuItem.classList.add('svg-loaded');
+        }
       }
     }
   });
