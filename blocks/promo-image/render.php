a80f711a19bae49fad862439b675148bfd30d10f
Steve Lambert <67271+slambert@users.noreply.github.com>
2024-07-23 23:12:44 -0400

    feat: dialing in the block

--- blocks/promo-image/render.php
+++ blocks/promo-image/render.php
@@ -1,13 +1,15 @@
 <?php
 // add code to display promo-image block:
 $image = get_field('promo-image', get_the_ID());
- 
-if( !empty( $image ) ): ?>
-    <img class="acf-promo-image" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+?>
+<div class="acf-promo-image-container">
+    <?php
+    if (!empty($image)): ?>
+        <img class="acf-promo-image" src="<?php echo esc_url($image['url']); ?>"
+            alt="<?php echo esc_attr($image['alt']); ?>" />
 
-<?php elseif (has_post_thumbnail()) : ?>
-    <p>no promo image</p>
-    <?php the_post_thumbnail('medium_large'); // add code to display a default image or message when no image is provided ?>
+    <?php elseif (has_post_thumbnail()): ?>
+        <?php the_post_thumbnail('medium_large'); // add code to display a default image or message when no image is provided ?>
 
-<?php endif; ?>
-	
+    <?php endif; ?>
+</div>
